e892724763897a6b2d339d9860c32951feb591bb
Ilya Bakhlin <git@ilyabakhlin.com>
2022-05-30 00:35:16 +0200

    Adding the files.

---
+++ webpack.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./source/TypeScript/index.tsx",
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader"
+            }
+        ]
+    },
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "public", "js")
+    },
+    resolve: {
+        extensions: [".tsx"]
+    }
+}
