0da22b2467d089f12a1246438edde0dc4c633f7d
Ilya Bakhlin <git@ilyabakhlin.com>
2022-05-30 00:35:17 +0200

    Adding some pages.

---
+++ source/TypeScript/Pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Home(): JSX.Element {
+    return (
+        <p>Home</p>
+    );
+}
--- source/TypeScript/index.tsx
+++ source/TypeScript/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Application } from "./Components/Application";
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { Characters } from "./Pages/Characters";
 
 window.addEventListener("load", (): void => {
-    const main: Element | null = window.document.querySelector("main#app");
+    const main: Element | null = window.document.querySelector("div#app");
 
     if (main instanceof Element) {
-        createRoot(main).render(<Application/>);
+        createRoot(main).render(
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/characters" element={<Characters/>}/>
+                </Routes>
+            </BrowserRouter>,
+        );
     } else {
         throw new Error("Something went wrong.");
     }
