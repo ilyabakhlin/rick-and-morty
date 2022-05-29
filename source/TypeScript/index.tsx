9e797777e7ef8e68b65a6dfb8c35bd03b016dd2b
Ilya Bakhlin <git@ilyabakhlin.com>
2022-05-30 00:35:16 +0200

    Installing and configuring React.

--- source/TypeScript/index.tsx
+++ source/TypeScript/index.tsx
@@ -1,3 +1,12 @@
-window.addEventListener("load", () => {
-    window.console.log("Hello, world!");
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+
+window.addEventListener("load", (): void => {
+    const main: Element | null = window.document.querySelector("main#app");
+
+    if (main instanceof Element) {
+        createRoot(main).render(<p>Hello, world!</p>);
+    } else {
+        throw new Error("Something went wrong.");
+    }
 });
